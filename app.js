4ad15847dffca85fecb674ee54eb25811dc76d6a
Greg Jopa <greg.jopa@yp.com>
2013-11-23 21:41:18 -0800

    Add express.js app

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+
+app.listen(3000);
+console.log('listening on port 3000');
