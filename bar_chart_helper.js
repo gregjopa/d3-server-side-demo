8dd38be878fbb80dfcdbdbdf59a40eff423fd207
Martin Connor <mepc36@gmail.com>
2022-10-16 11:26:47 -0400

    Upgrade tunes.js' d3 version.

--- bar_chart_helper.js
+++ bar_chart_helper.js
@@ -1,6 +1,8 @@
 const jsdom = require('jsdom');
 const d3 = Object.assign({}, require('d3-selection'));
 const barChart = require('./bar_chart');
+const { tune } = require('./tunes-viz-only')
+const { csv } = require('./csv')
 
 const { JSDOM } = jsdom;
 const document = new JSDOM().window.document;
@@ -16,6 +18,23 @@ function getBarChart(params) {
 
   const svg = d3.select(document.getElementById(containerId)).node().outerHTML;
   d3.select(document.getElementById(containerId)).remove();
+
+  var userviz = tune({
+    source: '',
+    file: 'userviz',
+    title: '',
+    artist: '',
+    album: '',
+    data: csv,
+    show: true,
+    music: false,
+    container: 'userviz',
+    missing_words: false
+  });
+  console.log('userviz:', userviz)
+  console.log('document:', document)
+  const svg2 = d3.select(document.getElementById('tunes-userviz'))
+  console.log('svg2:', svg2)
 
   return svg;
 }
